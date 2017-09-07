924e7670accbc36f639d325b2508e6558e0f7dff
Destiya <e_antz@yahoo.com>
2017-09-07 20:54:32 +0700

    add more info

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,9 +1,85 @@
 console.log(
-    "Congratulation!! You've found me on this page."
+    "Congratulation!! You've just found me on this page."
 );
+
+console.log('');
 
 console.log(
-    "Let's talk about me a little bit."
+    "Let's talk about me a little bit.\n" + 
+    "I started to code 7 years ago as a part-timer in a local software house. " +
+    "I code mostly using PHP. I utilize frameworks like Laravel and CodeIgniter " + 
+    "also CMSes like WordPress and Drupal.\n" +
+    "I'm currently looking for a new opportunity to advance my career and skill. " +
+    "Although PHP is my first choice language, I will accept any challenge to code in other language. " +
+    "Maybe in golang, nodejs, or even python?"
 );
 
+console.log('');
+
 console.log('What about my experiences?');
+console.log(
+    "MamiKos (https://mamikos.com)\n" + 
+    "January 2017 - present\n" +
+    "As backend developer, most of the time I will develop features, build API for apps, as well as maintain, refactor, " +
+    "and optimize existing codebase. Also, I allocate 10% of my time to contribute to our frontend codebase " +
+    "(adding small features, bugfixing simple layout, etc).\n" +
+    "Working in a startup is like working on your very own product, because you will give everything you have " +
+    "to make it working."
+);
+console.log(
+    "Closely Coded (http://closelycoded.com)\n" +
+    "November 2015 - December 2016\n" +
+    "Here, I contributed on various projects, most of them are Drupal based e-commerce websites. " +
+    "But, I also created some company profile websites using both Drupal and WordPress. " +
+    "It's fun to have various projects with various requirements."
+);
+console.log(
+    "Formulatrix Indonesia (http://formulatrix.com)\n" +
+    "July 2014 - October 2016\n" +
+    "In this lab automation company, I worked as Software Quality Assurance. " + 
+    "I did mostly manual testing for their internal web based software. " +
+    "The experience and skill that I learnt during this time are very useful to make me " +
+    "think more critical about a specific topic."
+);
+console.log(
+    "Wacana Inti Informatika\n" +
+    "2009 - 2010\n" + 
+    "My professional career started here, where I worked as part timer " + 
+    "then converted to full time Junior Web Developer. " +
+    "My day to day tasks were creating various websites for various clients.\n" +
+    "Sometimes I was so shy when I re-read the code I wrote during my employment here. " +
+    "That's so messy. But that's part of someone's journey, right?" 
+);
+
+console.log('');
+
+console.log("What I've done");
+console.log(
+    "Mamikos (https://mamikos.com)\n" + 
+    "#laravel #mariadb #restAPI"
+);
+console.log(
+    "Qualitatif (http://qualitatif.co)\n" + 
+    "#drupal7 #drupalcommerce #html #css #jquery"
+);
+console.log(
+    "Psynaptica (http://psynaptica.com)\n" + 
+    "#wordpress #html #css #jquery"
+);
+console.log(
+    "FDC Singapore (http://fdc.com.sg)\n" +
+    "#drupa7 #html #css #jquery"
+)
+console.log(
+    "Kelas Bahasa (http://kelasbahasa.com)\n" +
+    "#laravel #html #css #jquery"
+);
+
+console.log('');
+
+console.log('Contact me here : ')
+console.log('email: destiya.dian@gmail.com');
+console.log('facebook: https://facebook.com/destiya');
+console.log('twitter: https://twitter.com/eantz');
+console.log('linkedin: https://www.linkedin.com/in/destiya-wijayanto-48a66819/');
+console.log('github: https://github.com/eantz');
