08f91176b2faff831b0cde9e9fe703078fe302aa
Destiya <e_antz@yahoo.com>
2018-01-29 20:48:57 +0700

    add experience page

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import Header from './Header';
+
+import experienceData from './../experienceData';
 
 class Experiences extends React.Component {
+  constructor() {
+    super();
+
+    this.renderExperience = this.renderExperience.bind(this);
+  }
+
+  renderExperience(experience, i) {
+    return (
+      <li key={i}>
+        <div className="experience-year">{experience.year}</div>
+        <div className="experience-content">
+          <h4 className="experience-title">{experience.title}</h4>
+          <p className="experience-description">
+            {experience.description}
+          </p>
+        </div>
+      </li>
+    )
+  }
+
   render() {
     return (
-      <h1>This is Experiences Page</h1>
+      <div>
+        <Header sectionTitle="Experiences" />
+
+        <ul className="experience-timeline">
+          { experienceData.map((experience, i) => this.renderExperience(experience, i))  }
+        </ul>
+      </div>
+      
     )
   }
 }
---
+++ src/experienceData.js
@@ -0,0 +1,41 @@
+const experienceData = [
+  {
+    year: '2017',
+    title: 'Backend Developer at MamiKos',
+    description: 'Building Indonesia #1 Kost directory. I took backend role here and develop ' +
+      'various new exciting features that will help user find a kost easier.'
+  },
+  {
+    year: '2015',
+    title: 'Web Developer at Closely Coded',
+    description: 'In order to back to the track as developer, I took a new role as ' +
+      'web developer. Here I created various online stores and company profiles mostly ' +
+      'using Drupal. I did both backend and frontend development.'
+  },
+  {
+    year: '2014',
+    title: 'Software Quality Assurance at Formulatrix Indonesia',
+    description: 'Hunt down whether obvious and non obvious bugs. Execute and maintain test cases. ' +
+      'Make sure all new features and bugfix will not break previous working functions, also make sure ' +
+      'that they are match with the requirements.'
+  },
+  {
+    year: '2012',
+    title: 'Took Bachelor Degree on Information Technology',
+    description: 'Graduated on 2014 from Satya Wacana Christian University'
+  },
+  {
+    year: '2009',
+    title: 'Junior Web Developer at Wacana Inti Informatika',
+    description: 'While still studying at university, I took a part-time job as Junior Web Developer. ' + 
+      'This is my first ever job as developer. Later in mid 2010, I was converted to a ' + 
+      'full-time employee.'
+  },
+  {
+    year: '2007',
+    title: 'Took Diploma Degree on Information Technology',
+    description: 'Graduated on 2010 from Satya Wacana Christian University'
+  }
+];
+
+export default experienceData;
